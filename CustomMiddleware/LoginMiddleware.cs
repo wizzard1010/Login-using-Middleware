0c6a0f6c01be6c5f400dc1eb55f7b11130bf5ae4
wizzard1010 <thuutaag2810@gmail.com>
2024-02-09 16:02:39 +0630

    first commit

---
+++ CustomMiddleware/LoginMiddleware.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Login_using_Middleware.CustomMiddleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoginMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/" && context.Request.Method == "POST")
+            {
+                //read response body
+                StreamReader reader = new StreamReader(context.Request.Body);
+                string body = await reader.ReadToEndAsync();
+
+                //string to dictionary
+                Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+
+                string? email = null, password = null;
+
+                //read firstname
+                if (queryDict.ContainsKey("email"))
+                {
+                    email = Convert.ToString(queryDict["email"][0]);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid 'email'");
+                }
+                if (queryDict.ContainsKey("password"))
+                {
+                    password = Convert.ToString(queryDict["password"][0]);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid 'password'");
+                }
+                if (!queryDict.ContainsKey("email") && !queryDict.ContainsKey("password"))
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid request");
+                }
+
+                string validEmail = "admin@example.com", validPassword = "admin1234";
+                bool isValidLogin;
+
+                //if email and password are valid
+                if (email == validEmail && password == validPassword)
+                {
+                    isValidLogin = true;
+                }
+                else
+                {
+                    isValidLogin = false;
+                }
+
+                //send response
+                if (isValidLogin)
+                {
+                    await context.Response.WriteAsync("Successful login");
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid Login");
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LoginMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoginMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoginMiddleware>();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Login_using_Middleware.CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseLoginMiddleware();
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("No Response");
+});
+
